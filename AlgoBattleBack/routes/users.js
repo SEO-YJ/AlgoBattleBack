29961728d0521e2c19b6d8142be5af877759b06f
Youjun Seo <76039485+SEO-YJ@users.noreply.github.com>
2024-02-22 17:46:29 +0900

    [mod] users post api 수정 완료

--- AlgoBattleBack/routes/users.js
+++ AlgoBattleBack/routes/users.js
@@ -4,107 +4,58 @@ let User = require("../model/User");
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-/* POST: 백준 유저 정보 생성 */
-// router.post("/:userid", function (req, res, next) {
-//   getSolvedacUserData(req.params.userid).then((user) => {
-//     User.create({
-//       handle: user.handle,
-//       tier: user.tier,
-//       solvedCount: user.solvedCount,
-//     })
-//       .then((user) => {
-//         res.json(user);
-//       })
-//       .catch((err) => {
-//         next(err);
-//       });
-//   });
-// });
-
 /* POST: 백준 유저 정보 생성 또는 업데이트 */
-router.post("/:userid", function (req, res, next) {
-  // getSolvedacUserData 함수는 해당 userid의 사용자 정보를 반환하는 함수로 가정합니다.
-  getSolvedacUserData(req.params.userid)
-    .then((user) => {
-      // 사용자가 없을 경우 null로 응답
-      if (!user) {
-        // 사용자 정보가 없을 경우 null을 응답
-        console.log("사용자 정보가 없습니다.");
-        res.json(null);
-        return;
-      }
-      return user;
-    })
-    .then((user) => {
-      getUserSolvedProblems(req.params.userid)
-        .then((solvedProblemsList) => {
-          if (!solvedProblemsList) {
-            // 사용자가 푼 문제 정보 없을 경우 null을 응답
-            console.log("사용자가 푼 문제 정보가 없습니다.");
-            res.json(null);
-            return;
-          }
-          // 사용자 정보 객체 생성
-          const userData = {
-            handle: user.handle,
-            tier: user.tier,
-            solvedCount: user.solvedCount,
-            solvedProblemsList,
-          };
-          return userData;
-        })
-        .then((userData) => {
-          // 해당 userid로 이미 데이터가 존재하는지 확인
-          User.findOne({ handle: req.params.userid }).then((existingUser) => {
-            if (existingUser) {
-              // 이미 데이터가 존재한다면 업데이트
-              User.findOneAndUpdate(
-                { handle: req.params.userid },
-                userData,
-                { new: true } // 업데이트된 데이터를 반환하도록 설정
-              )
-                .then((updatedUser) => {
-                  console.log("사용자 정보 업데이트");
-                  res.json(updatedUser); // 업데이트된 사용자 정보 반환
-                })
-                .catch((err) => {
-                  next(err); // 오류 발생 시 다음 미들웨어에 전달
-                });
-            } else {
-              // 데이터가 존재하지 않는다면 새로 생성
-              User.create(userData)
-                .then((newUser) => {
-                  console.log("사용자 새로 생성");
-                  res.json(newUser); // 새로 생성된 사용자 정보 반환
-                })
-                .catch((err) => {
-                  next(err); // 오류 발생 시 다음 미들웨어에 전달
-                });
-            }
-          });
-        });
-    });
+router.post("/:userid", async function (req, res, next) {
+  try {
+    const user = await Promise.all([
+      getSolvedacUserData(req.params.userid),
+      getUserSolvedProblems(req.params.userid),
+    ]);
+    // 유저 정보와 맞은 문제들 잘 가져왔나 찍어봄
+    console.log(user);
+    const [userInfo, solvedProblemsList] = user;
+
+    // 사용자가 없을 경우 null로 응답
+    if (!userInfo || !solvedProblemsList) {
+      console.log("사용자 정보 또는 푼 문제 정보가 없습니다.");
+      res.json(null);
+      return;
+    }
+
+    // 사용자 정보 객체 생성
+    const userData = {
+      handle: userInfo.handle,
+      tier: userInfo.tier,
+      solvedCount: userInfo.solvedCount,
+      solvedProblemsList,
+    };
+
+    // 해당 userid로 이미 데이터가 존재하는지 확인
+    const existingUser = await User.findOne({ handle: req.params.userid });
+
+    if (existingUser) {
+      // 이미 데이터가 존재한다면 업데이트
+      const updatedUser = await User.findOneAndUpdate(
+        { handle: req.params.userid },
+        userData,
+        { new: true }
+      );
+      console.log("사용자 정보 업데이트");
+      res.json(updatedUser);
+    } else {
+      // 데이터가 존재하지 않는다면 새로 생성
+      const newUser = await User.create(userDataObject);
+      console.log("사용자 새로 생성");
+      res.json(newUser);
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
 
 // Solved AC -> User 데이터 가져오기
-// async function getSolvedacUserData(USER_ID) {
-//   let response = await fetch(
-//     `https://api-py.vercel.app/?r=https://solved.ac/api/v3/user/show?handle=${USER_ID}`,
-//     {
-//       method: "GET",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     }
-//   );
-
-//   let data = await response.json();
-//   console.log(data);
-//   return data;
-// }
-
 async function getSolvedacUserData(USER_ID) {
   try {
     let response = await fetch(
@@ -118,7 +69,6 @@ async function getSolvedacUserData(USER_ID) {
     );
 
     let data = await response.json();
-    console.log(data);
 
     // 만약 사용자 정보가 없을 경우 에러를 throw하여 잡습니다.
     if (response.status === 404 || !data || data.error) {
@@ -133,29 +83,94 @@ async function getSolvedacUserData(USER_ID) {
   }
 }
 
+// 백준 맞은 문제들 가져오기
 async function getUserSolvedProblems(USER_ID) {
   const headers = {
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
   };
-
-  axios
-    .get(`https://www.acmicpc.net/user/${USER_ID}`, { headers })
-    .then((response) => {
-      // 요청 성공 시 처리할 로직
-      const $ = cheerio.load(response.data);
-      const result = $("div.problem-list a")
-        .map((i, elem) => {
-          const problemNumber = $(elem).text();
-          return problemNumber;
-        })
-        .get();
-      // console.log(result);
-      return result;
-    })
-    .catch((error) => {
-      // 요청 실패 시 처리할 로직
-      // console.error(error);
-      return null;
-    });
+  try {
+    const response = await axios.get(
+      `https://www.acmicpc.net/user/${USER_ID}`,
+      { headers }
+    );
+    const $ = cheerio.load(response.data);
+    const result = $("div.problem-list a")
+      .map((i, elem) => {
+        const problemNumber = $(elem).text();
+        return problemNumber;
+      })
+      .get();
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
+
+//// 이전 콜백으로 작성하여
+//// 두 함수가 데이터를 가져오기 전에 에러 발생
+//// Promise - await로 해결
+// router.post("/:userid", function (req, res, next) {
+//   // getSolvedacUserData 함수는 해당 userid의 사용자 정보를 반환하는 함수로 가정합니다.
+//   getSolvedacUserData(req.params.userid)
+//     .then((user) => {
+//       // 사용자가 없을 경우 null로 응답
+//       if (!user) {
+//         // 사용자 정보가 없을 경우 null을 응답
+//         console.log("사용자 정보가 없습니다.");
+//         res.json(null);
+//         return;
+//       }
+//       return user;
+//     })
+//     .then((user) => {
+//       getUserSolvedProblems(req.params.userid)
+//         .then((solvedProblemsList) => {
+//           if (!solvedProblemsList) {
+//             // 사용자가 푼 문제 정보 없을 경우 null을 응답
+//             console.log("사용자가 푼 문제 정보가 없습니다.");
+//             res.json(null);
+//             return;
+//           }
+//           // 사용자 정보 객체 생성
+//           const userData = {
+//             handle: user.handle,
+//             tier: user.tier,
+//             solvedCount: user.solvedCount,
+//             solvedProblemsList,
+//           };
+//           return userData;
+//         })
+//         .then((userData) => {
+//           // 해당 userid로 이미 데이터가 존재하는지 확인
+//           User.findOne({ handle: req.params.userid }).then((existingUser) => {
+//             if (existingUser) {
+//               // 이미 데이터가 존재한다면 업데이트
+//               User.findOneAndUpdate(
+//                 { handle: req.params.userid },
+//                 userData,
+//                 { new: true } // 업데이트된 데이터를 반환하도록 설정
+//               )
+//                 .then((updatedUser) => {
+//                   console.log("사용자 정보 업데이트");
+//                   res.json(updatedUser); // 업데이트된 사용자 정보 반환
+//                 })
+//                 .catch((err) => {
+//                   next(err); // 오류 발생 시 다음 미들웨어에 전달
+//                 });
+//             } else {
+//               // 데이터가 존재하지 않는다면 새로 생성
+//               User.create(userData)
+//                 .then((newUser) => {
+//                   console.log("사용자 새로 생성");
+//                   res.json(newUser); // 새로 생성된 사용자 정보 반환
+//                 })
+//                 .catch((err) => {
+//                   next(err); // 오류 발생 시 다음 미들웨어에 전달
+//                 });
+//             }
+//           });
+//         });
+//     });
+// });
